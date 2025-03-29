98d729ec84f8cb5e3e6d5e210e60ca649b788822
shoebabedin12 <shoebabedin12@gmail.com>
2025-03-30 02:11:53 +0600

    update

---
+++ src/Animals.vue
@@ -0,0 +1,1 @@
+<template>animals page</template>
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import './style.css'
+
+import App from './App.vue'
+import Animals from './Animals.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/animals', component: Animals },
+    ]
+});
+
+const app = createApp(App)
+
+app.use(router);
+
+app.mount('#app')
