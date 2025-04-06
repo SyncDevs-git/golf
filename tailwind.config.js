a0d1f5a486fc07184cb0466093ccfa1387894c1a
shoebabedin <shoeb.abedin@intelli.global>
2025-04-06 19:01:59 +0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,13 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+     'styrene-medium': ['Styrene-A-Web', 'sans-serif'],
+    },
     extend: {
       colors: {
         primary: "#FFA300",
+        secondary: "#FFFFFF4D",
       },
     },
   },
