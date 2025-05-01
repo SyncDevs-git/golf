a9184b5a3c624d5366ef48f8fffd68c50ddcaf2a
shoebabedin12 <shoebabedin12@gmail.com>
2025-05-02 03:34:32 +0600

    update

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-[#e7eaea] max-md:overflow-x-hidden max-md:pt-[120px]">
+    <div class="bg-[#e7eaea] max-md:overflow-x-hidden max-md:pt-[120px] max-lg:pb-[70px] pb-[30px]">
         <AppHeader />
         <NuxtPage />
         <!-- <AppFooter /> -->
